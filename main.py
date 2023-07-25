8b96e271c0a2cc363bef410afb2617c0b9b5491e
mohitchauhan3 <101303720+mohitchauhan3@users.noreply.github.com>
2023-07-25 07:47:00 +0000

    loop the the amount of ingredients needed times the amount  to cost per units

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ def recipe_calcuator():
   total_cost = 0.0
   
   num_ingredients = int(input("enter the number of ingredients: "))
+  
+  for i in range(num_ingredients):
+    ingredients_name = input(f"enter the name of ingredients {i + 1
+    }: ") 
+    amount = float(input(f"enter the amunt of {ingredients_name} needed (in units): "))
+    ingredinet_cost = amount * cost_per_unit
+    total_cost += ingredient_cost
+  
